85f01fab8936fa61b0c70331b4e084b6cdfdefb5
magicsong <magicsong@yunify.com>
2019-03-08 12:34:23 +0800

    more tests

--- pkg/controller/s2irun/s2irun_controller_test.go
+++ pkg/controller/s2irun/s2irun_controller_test.go
@@ -17,104 +17,111 @@ limitations under the License.
 package s2irun
 
 import (
-	"os"
-	"testing"
+	"context"
+	"fmt"
 	"time"
 
 	devopsv1alpha1 "github.com/kubesphere/s2ioperator/pkg/apis/devops/v1alpha1"
-	"github.com/onsi/gomega"
-	"golang.org/x/net/context"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-var c client.Client
+var _ = Describe("Test reconcile", func() {
 
-var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: "default"}}
-var depKey = types.NamespacedName{Name: "foo-job", Namespace: "default"}
-var cmKey = types.NamespacedName{Name: "foo-configmap", Namespace: "default"}
+	var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: "default"}}
+	var depKey = types.NamespacedName{Name: "foo-job", Namespace: "default"}
+	var cmKey = types.NamespacedName{Name: "foo-configmap", Namespace: "default"}
+	const timeout = time.Second * 10
 
-const timeout = time.Second * 50
-
-func TestReconcile(t *testing.T) {
-	g := gomega.NewGomegaWithT(t)
-	instance := &devopsv1alpha1.S2iRun{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"},
-		Spec: devopsv1alpha1.S2iRunSpec{
-			BuilderName: "foo",
-		},
-	}
-
-	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
-	// channel when it is finished.
-	mgr, err := manager.New(cfg, manager.Options{})
-	g.Expect(err).NotTo(gomega.HaveOccurred())
-	c = mgr.GetClient()
-
-	recFn, requests := SetupTestReconcile(newReconciler(mgr))
-	g.Expect(add(mgr, recFn)).NotTo(gomega.HaveOccurred())
-
-	stopMgr, mgrStopped := StartTestManager(mgr, g)
-
-	defer func() {
-		close(stopMgr)
-		mgrStopped.Wait()
-	}()
-	//setup env
-
-	os.Setenv("S2IIMAGENAME", "S2IIMAGENAME/S2IIMAGENAME")
-	//create a s2ibuilder
-
-	s2ibuilder := &devopsv1alpha1.S2iBuilder{
-		ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"},
-		Spec: devopsv1alpha1.S2iBuilderSpec{
-			Config: &devopsv1alpha1.S2iConfig{
-				ImageName: "hello/world",
-				Tag:       "latest",
+	It("Should get job and configmap when everything is right", func() {
+		instance := &devopsv1alpha1.S2iRun{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"},
+			Spec: devopsv1alpha1.S2iRunSpec{
+				BuilderName: "foo",
 			},
-		},
-	}
-	err = c.Create(context.TODO(), s2ibuilder)
-	g.Expect(err).NotTo(gomega.HaveOccurred())
-	defer c.Delete(context.TODO(), s2ibuilder)
-	// Create the S2iRun object and expect the Reconcile and Deployment to be created
-	err = c.Create(context.TODO(), instance)
-	// The instance object may not be a valid object because it might be missing some required fields.
-	// Please modify the instance object by adding required fields and then remove the following if statement.
-	if apierrors.IsInvalid(err) {
-		t.Logf("failed to create object, got an invalid object error: %v", err)
-		return
-	}
-	g.Expect(err).NotTo(gomega.HaveOccurred())
-	defer c.Delete(context.TODO(), instance)
-	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
+		}
+		//create a s2ibuilder
 
-	//configmap
-	cm := &corev1.ConfigMap{}
-	g.Eventually(func() error { return c.Get(context.TODO(), cmKey, cm) }, timeout).
-		Should(gomega.Succeed())
-	g.Expect(c.Delete(context.TODO(), cm)).NotTo(gomega.HaveOccurred())
-	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
-	g.Eventually(func() error { return c.Get(context.TODO(), cmKey, cm) }, timeout).
-		Should(gomega.Succeed())
-	defer c.Delete(context.TODO(), cm)
+		s2ibuilder := &devopsv1alpha1.S2iBuilder{
+			ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"},
+			Spec: devopsv1alpha1.S2iBuilderSpec{
+				Config: &devopsv1alpha1.S2iConfig{
+					ImageName: "hello/world",
+					Tag:       "latest",
+				},
+			},
+		}
+		err := c.Create(context.TODO(), s2ibuilder)
+		Expect(err).NotTo(HaveOccurred())
+		defer c.Delete(context.TODO(), s2ibuilder)
+		// Create the S2iRun object and expect the Reconcile and Deployment to be created
+		err = c.Create(context.TODO(), instance)
+		// The instance object may not be a valid object because it might be missing some required fields.
+		// Please modify the instance object by adding required fields and then remove the following if statement.
+		if apierrors.IsInvalid(err) {
+			fmt.Printf("failed to create object, got an invalid object error: %v", err)
+			return
+		}
+		Expect(err).NotTo(HaveOccurred())
+		defer c.Delete(context.TODO(), instance)
+		Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
 
-	job := &batchv1.Job{}
-	g.Eventually(func() error { return c.Get(context.TODO(), depKey, job) }, timeout).
-		Should(gomega.Succeed())
+		//configmap
+		cm := &corev1.ConfigMap{}
+		Eventually(func() error { return c.Get(context.TODO(), cmKey, cm) }, timeout).
+			Should(Succeed())
+		Expect(c.Delete(context.TODO(), cm)).NotTo(HaveOccurred())
+		Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+		Eventually(func() error { return c.Get(context.TODO(), cmKey, cm) }, timeout).
+			Should(Succeed())
+		defer c.Delete(context.TODO(), cm)
 
-	// Delete the Deployment and expect Reconcile to be called for Deployment deletion
-	g.Expect(c.Delete(context.TODO(), job)).NotTo(gomega.HaveOccurred())
-	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
-	g.Eventually(func() error { return c.Get(context.TODO(), depKey, job) }, timeout).
-		Should(gomega.Succeed())
+		job := &batchv1.Job{}
+		Eventually(func() error { return c.Get(context.TODO(), depKey, job) }, timeout).
+			Should(Succeed())
 
-	// Manually delete Deployment since GC isn't enabled in the test control plane
-	g.Expect(c.Delete(context.TODO(), job)).To(gomega.Succeed())
+		// Delete the Deployment and expect Reconcile to be called for Deployment deletion
+		Expect(c.Delete(context.TODO(), job)).NotTo(HaveOccurred())
+		Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+		Eventually(func() error { return c.Get(context.TODO(), depKey, job) }, timeout).
+			Should(Succeed())
 
-}
+		// Manually delete Deployment since GC isn't enabled in the test control plane
+		Expect(c.Delete(context.TODO(), job)).To(Succeed())
+	})
+	It("Should not config from nonexist templates", func() {
+		instance := &devopsv1alpha1.S2iRun{ObjectMeta: metav1.ObjectMeta{Name: "foo1", Namespace: "default"},
+			Spec: devopsv1alpha1.S2iRunSpec{
+				BuilderName: "foo1",
+			},
+		}
+		//create a s2ibuildern using template
+		s2ibuilder := &devopsv1alpha1.S2iBuilder{
+			ObjectMeta: metav1.ObjectMeta{Name: "foo1", Namespace: "default"},
+			Spec: devopsv1alpha1.S2iBuilderSpec{
+				FromTemplate: &devopsv1alpha1.UserDefineTemplate{
+					Name: "NotExsit",
+				},
+				Config: &devopsv1alpha1.S2iConfig{
+					Tag: "latest",
+				},
+			},
+		}
+		err := c.Create(context.TODO(), s2ibuilder)
+		Expect(err).NotTo(HaveOccurred())
+		defer c.Delete(context.TODO(), s2ibuilder)
+
+		err = c.Create(context.TODO(), instance)
+		Expect(err).NotTo(HaveOccurred())
+		defer c.Delete(context.TODO(), instance)
+		//configmap
+		cm := &corev1.ConfigMap{}
+		Eventually(func() error { return c.Get(context.TODO(), cmKey, cm) }, timeout).
+			ShouldNot(Succeed())
+	})
+})
