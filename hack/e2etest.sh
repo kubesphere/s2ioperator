502540212abeb67c7e5308c612508305e9565b67
magicsong <magicsong@yunify.com>
2019-03-14 14:44:47 +0800

    sed broken in linux

--- hack/e2etest.sh
+++ hack/e2etest.sh
@@ -16,10 +16,15 @@ trap cleanup EXIT SIGINT SIGQUIT
 docker build -f deploy/Dockerfile -t ${IMG} bin/
 docker push $IMG
 echo "updating kustomize image patch file for manager resource"
-sed -i '' -e 's@image: .*@image: '"${IMG}"'@' ./config/default/manager_image_patch.yaml
 
+if [ "$(uname)" == "Darwin" ]; then
+    sed -i '' -e 's@image: .*@image: '"${IMG}"'@' ./config/default/manager_image_patch.yaml
+    sed -i '' -e  's/namespace: .*/namespace: '"${TEST_NS}"'/' ./config/default/kustomization.yaml
+else
+    sed -i  -e 's@image: .*@image: '"${IMG}"'@' ./config/default/manager_image_patch.yaml
+    sed -i  -e  's/namespace: .*/namespace: '"${TEST_NS}"'/' ./config/default/kustomization.yaml
+fi
 kubectl create ns  $TEST_NS
-sed -i '' -e  's/namespace: .*/namespace: '"${TEST_NS}"'/' ./config/default/kustomization.yaml
 kustomize build config/default -o $dest
 kubectl apply -f $dest
 ./hack/certs.sh --service webhook-server-service --namespace $TEST_NS --secret webhook-server-secret
