8a04ccff846c6133ce1aa6f52ec2904ebebf91ad
runzexia <runzexia@yunify.com>
2019-03-22 17:34:49 +0800

    add secret support (#61)
    * add e2e test
    
    * Revert "Revert "add docker secret support (#59)""
    
    This reverts commit a9dd13a560340bba029f02f7c5116d736885cb92.

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -214,4 +214,79 @@ var _ = Describe("", func() {
 		}, time.Minute*5, time.Second*10).Should(Succeed())
 	})
 
+	It("Should work well when using secrets", func() {
+		//create a s2ibuilder
+		cleanDelete := client.PropagationPolicy(metav1.DeletePropagationBackground)
+		secret := &corev1.Secret{}
+		reader, err := os.Open(workspace + "/config/samples/secret/secret.yaml")
+		Expect(err).NotTo(HaveOccurred(), "Cannot read sample yamls")
+		err = yaml.NewYAMLOrJSONDecoder(reader, 10).Decode(secret)
+		Expect(err).NotTo(HaveOccurred(), "Cannot unmarshal yamls")
+		err = testClient.Create(context.TODO(), secret)
+		Expect(err).NotTo(HaveOccurred())
+		defer testClient.Delete(context.TODO(), secret)
+
+		s2ibuilder := &devopsv1alpha1.S2iBuilder{}
+		reader, err = os.Open(workspace + "/config/samples/secret/devops_v1alpha1_s2ibuilder.yaml")
+		Expect(err).NotTo(HaveOccurred(), "Cannot read sample yamls")
+		err = yaml.NewYAMLOrJSONDecoder(reader, 10).Decode(s2ibuilder)
+		Expect(err).NotTo(HaveOccurred(), "Cannot unmarshal yamls")
+		err = testClient.Create(context.TODO(), s2ibuilder)
+		Expect(err).NotTo(HaveOccurred())
+		defer testClient.Delete(context.TODO(), s2ibuilder)
+		// Create the S2iRun object and expect the Reconcile and Deployment to be created
+		s2irun := &devopsv1alpha1.S2iRun{}
+		reader, err = os.Open(workspace + "/config/samples/secret/devops_v1alpha1_s2irun.yaml")
+		Expect(err).NotTo(HaveOccurred(), "Cannot read sample yamls")
+		err = yaml.NewYAMLOrJSONDecoder(reader, 10).Decode(s2irun)
+		Expect(err).NotTo(HaveOccurred(), "Cannot unmarshal yamls")
+		err = testClient.Create(context.TODO(), s2irun)
+		Expect(err).NotTo(HaveOccurred())
+		defer testClient.Delete(context.TODO(), s2irun, cleanDelete)
+
+		createdInstance := &devopsv1alpha1.S2iRun{}
+		Eventually(func() error {
+			return testClient.Get(context.TODO(), types.NamespacedName{Name: s2irun.Name, Namespace: s2irun.Namespace}, createdInstance)
+		}, timeout).Should(Succeed())
+
+		instanceUidSlice := strings.Split(string(createdInstance.UID), "-")
+		var cmKey = types.NamespacedName{
+			Name:      s2irun.Name + fmt.Sprintf("-%s", instanceUidSlice[len(instanceUidSlice)-1]) + "-configmap",
+			Namespace: s2irun.Namespace}
+		var depKey = types.NamespacedName{
+			Name:      s2irun.Name + fmt.Sprintf("-%s", instanceUidSlice[len(instanceUidSlice)-1]) + "-job",
+			Namespace: s2irun.Namespace}
+		//configmap
+		cm := &corev1.ConfigMap{}
+		Eventually(func() error { return testClient.Get(context.TODO(), cmKey, cm) }, timeout).
+			Should(Succeed())
+		Expect(testClient.Delete(context.TODO(), cm)).NotTo(HaveOccurred())
+		Eventually(func() error { return testClient.Get(context.TODO(), cmKey, cm) }, timeout).
+			Should(Succeed())
+		defer testClient.Delete(context.TODO(), cm)
+
+		job := &batchv1.Job{}
+		Eventually(func() error { return testClient.Get(context.TODO(), depKey, job) }, timeout, time.Second).
+			Should(Succeed())
+
+		//for our example, the status must be successful
+		Eventually(func() error {
+			err = testClient.Get(context.TODO(), depKey, job)
+			if err != nil {
+				return err
+			}
+			if job.Status.Succeeded == 1 {
+				//Status of s2ibuilder should update too
+				tempBuilder := &devopsv1alpha1.S2iBuilder{}
+				err = testClient.Get(context.TODO(), types.NamespacedName{Name: s2ibuilder.Name, Namespace: s2ibuilder.Namespace}, tempBuilder)
+				if err != nil {
+					return err
+				}
+				if tempBuilder.Status.LastRunName != nil && *tempBuilder.Status.LastRunName == s2irun.Name && tempBuilder.Status.LastRunState == devopsv1alpha1.Successful && tempBuilder.Status.RunCount == 1 {
+					return nil
+				}
+			}
+			return fmt.Errorf("Failed")
+		}, time.Minute*5, time.Second*10).Should(Succeed())
+	})
 })
