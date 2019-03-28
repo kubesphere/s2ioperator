db7eeb70552e3418756a47a1ec5b00660403399c
runzexia <runzexia@yunify.com>
2019-03-28 17:29:18 +0800

    add s2ir validate & changelog

--- pkg/webhook/default_server/add_validating.go
+++ pkg/webhook/default_server/add_validating.go
@@ -21,6 +21,7 @@ import (
 
 	s2iBuilderValidating "github.com/kubesphere/s2ioperator/pkg/webhook/default_server/s2ibuilder/validating"
 	s2iBuilderTemplateValidating "github.com/kubesphere/s2ioperator/pkg/webhook/default_server/s2ibuildertemplate/validating"
+	s2iRunValidating "github.com/kubesphere/s2ioperator/pkg/webhook/default_server/s2irun/validating"
 )
 
 func init() {
@@ -56,6 +57,30 @@ func init() {
 	}
 
 	for k, v := range s2iBuilderTemplateValidating.HandlerMap {
+		_, found := HandlerMap[k]
+		if found {
+			log.V(1).Info(fmt.Sprintf(
+				"conflicting webhook builder names in handler map: %v", k))
+		}
+		_, found = builderMap[k]
+		if !found {
+			log.V(1).Info(fmt.Sprintf(
+				"can't find webhook builder name %q in builder map", k))
+			continue
+		}
+		HandlerMap[k] = v
+	}
+
+	for k, v := range s2iRunValidating.Builders {
+		_, found := builderMap[k]
+		if found {
+			log.V(1).Info(fmt.Sprintf(
+				"conflicting webhook builder names in builder map: %v", k))
+		}
+		builderMap[k] = v
+	}
+
+	for k, v := range s2iRunValidating.HandlerMap {
 		_, found := HandlerMap[k]
 		if found {
 			log.V(1).Info(fmt.Sprintf(
