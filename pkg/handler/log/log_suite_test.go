505813c5be644dbc1dc72b70396fc05d04021cbe
runzexia <runzexia@yunify.com>
2019-04-14 14:58:02 +0800

    fix ut

--- pkg/handler/log/log_suite_test.go
+++ pkg/handler/log/log_suite_test.go
@@ -18,6 +18,6 @@ var _ = Describe("Testing logging URL for kubesphere", func() {
 		logger := log.GetKubesphereLogger()
 		str, err := logger.GetURLOfPodLog("default", "pod-1")
 		Expect(err).ShouldNot(HaveOccurred())
-		Expect(str).To(Equal("http://ks-apigateway.kubesphere-system.svc/apis/logging.kubesphere.io/v1alpha2/namespaces/default/pods/pod-1?operation=query"))
+		Expect(str).To(Equal("http://ks-apigateway.kubesphere-system.svc/kapis/logging.kubesphere.io/v1alpha2/namespaces/default/pods/pod-1?operation=query"))
 	})
 })
