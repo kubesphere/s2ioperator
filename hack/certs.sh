4f3a3d04456ddeea20cc14c133727cb7995dc919
jackyu <jackyu@yunify.com>
2024-02-20 16:10:12 +0800

    update certs.sh that generate certs with DNS

--- hack/certs.sh
+++ hack/certs.sh
@@ -48,8 +48,20 @@ csrName=${service}.${namespace}
 CERTSDIR="config/certs"
 
 if [ ! -d ${CERTSDIR} ]; then
-  mkdir ${CERTSDIR}
+  mkdir -p ${CERTSDIR}
 fi
+
+cat > v3.ext <<-EOF
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1=${service}.${namespace}.svc
+DNS.2=${service}.${namespace}
+DNS.3=hostname
+EOF
 
 echo "creating certs in certsdir ${CERTSDIR} "
 
@@ -64,4 +76,4 @@ openssl genrsa -out ${CERTSDIR}/server.key 2048
 
 # create server.crt
 openssl req -new -sha256 -key ${CERTSDIR}/server.key -subj "/C=CN/ST=HB/O=QC/CN=${service}.${namespace}.svc" -out ${CERTSDIR}/server.csr
-openssl x509 -req -in ${CERTSDIR}/server.csr -CA ${CERTSDIR}/ca.crt -CAkey ${CERTSDIR}/ca.key -CAcreateserial -out ${CERTSDIR}/server.crt -days 10000 -sha256
+openssl x509 -req -in ${CERTSDIR}/server.csr -extfile v3.ext -CA ${CERTSDIR}/ca.crt -CAkey ${CERTSDIR}/ca.key -CAcreateserial -out ${CERTSDIR}/server.crt -days 10000 -sha256
